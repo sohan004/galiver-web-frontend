62b1189c06d7b0596fba53b73b1e897ecf05a50c
sohan004 <aryansohan02@gmail.com>
2024-03-10 01:24:49 +0600

    Changes committed on 2024-03-10 01:24:49

--- src/Layout/LayoutWithNavSidebar/LayoutWithNavSidebar.jsx
+++ src/Layout/LayoutWithNavSidebar/LayoutWithNavSidebar.jsx
@@ -17,9 +17,9 @@ const LayoutWithNavSidebar = () => {
 
                 <div className='w-full flex items-start gap-3'>
 
-                    <div onClick={() => setShowLeftSideBar(prev => !prev)} className={`fixed top-0 block md:hidden left-0 w-full h-full bg-black opacity-50 z-[50] backdrop-blur-md ${showLeftSideBar ? 'hidden' : 'block'}`}></div>
+                    <div onClick={() => setShowLeftSideBar(prev => !prev)} className={`fixed top-0 block md:hidden left-0 w-full h-full bg-black opacity-50 z-[100] backdrop-blur-md ${showLeftSideBar ? 'hidden' : 'block'}`}></div>
 
-                    <div className={`h-screen fixed md:sticky top-0 duration-500 w-[250px]  overflow-hidden  z-50 md:z-10 ${showLeftSideBar ? 'md:ml-0 -ml-[262px]' : 'ml-[0] md:-ml-[262px]'}`}>
+                    <div className={`h-screen fixed md:sticky top-0 duration-500 w-[250px]  overflow-hidden  z-[100] md:z-10 ${showLeftSideBar ? 'md:ml-0 -ml-[262px]' : 'ml-[0] md:-ml-[262px]'}`}>
                         <LeftSidebar></LeftSidebar>
                     </div>
                     {/* left sidebar end */}
