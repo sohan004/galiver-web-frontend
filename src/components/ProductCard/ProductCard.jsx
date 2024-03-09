728bcbcf15d3827dec3bd755baaf9be497875a53
sohan004 <aryansohan02@gmail.com>
2024-03-10 01:05:41 +0600

    Changes committed on 2024-03-10 01:05:41

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -5,6 +5,7 @@ import '@smastrom/react-rating/style.css'
 import { useNavigate } from 'react-router-dom';
 import getMedia from '../../utilities/getMedia';
 import getParentage from '../../utilities/getParcantage';
+import logo from '../../assets/logo/png-02.png'
 
 const ProductCard = ({ product }) => {
     const {
@@ -21,8 +22,10 @@ const ProductCard = ({ product }) => {
         <>
             <div onClick={() => navigate('/product/dsfdfsf')} className='w-full relative bg-white p-2 cursor-pointer  group hover:scale-105 duration-150 hover:shadow-lg'>
                 <div className='h-36 lg:h-56 w-full overflow-hidden relative'>
-                    <img src={getMedia(media?.name)} className='w-full h-full group-hover:scale-125 duration-300 ' alt="" />
-                    {discount > 0 && <p className='text-xs absolute top-0 left-0 bg-orange-600 text-white py-[2px] rounded-se-lg rounded-es-md px-1'><s className='opacity-60'>৳{discount}</s> <span className=''>-{getParentage(discount, price)}%</span></p>}
+                    <img src={getMedia(media?.name)} className='w-full h-full group-hover:scale-125 duration-300 z-40 relative ' alt="" />
+                    <div className='absolute z-30 bg-gradient-to-r from-orange-100 animate-pulse top-0 left-0 w-full h-full flex justify-center items-center'>
+                        <img src={logo} className='w-16 md:w-20' alt="" /></div>
+                    {discount > 0 && <p className='text-xs absolute top-0 left-0 bg-orange-600 text-white py-[2px] rounded-se-lg rounded-es-md px-1 z-50'><s className='opacity-60'>৳{discount}</s> <span className=''>-{getParentage(discount, price)}%</span></p>}
                 </div>
                 <p className='text-sm  mt-2'>{title}</p>
                 <div className='flex gap-1 items-center'>
