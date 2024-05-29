105de2ad9fd835f582a4ded5a94e005db7360f95
sohan004 <aryansohan02@gmail.com>
2024-05-29 18:19:48 +0600

    Changes committed on 2024-05-29 18:19:48

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
 import App from './App.jsx'
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: import.meta.env.VITE_GTM_ID // Replace with your GTM ID
+};
+
+TagManager.initialize(tagManagerArgs);
 
 // node version 20.7.0
 // block z index 1000000, 10000000, 9999, 99999
